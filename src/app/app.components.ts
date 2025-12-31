417b3ba7a8b9db46183fc45b9176c62a6a948d01
Pallavi05p <pallavidhanwe123@gmail.com>
2025-12-31 18:36:20 +0530

    fidcfvgbhnjmk,l

--- src/app/app.components.ts
+++ src/app/app.components.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { provideRouter, RouterModule, Routes } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
 import { AboutComponent } from './about/about';
 import { ContactComponent } from './contact/contact';
+import { Component } from '@angular/core';
 
+// Optional: NotFoundComponent
 @Component({
-  selector: 'app-root',
+  selector: 'app-not-found',
+  template: `<h1>Page Not Found</h1>`,
   standalone: true,
-  imports: [AboutComponent,ContactComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class NotFoundComponent {}
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'about', pathMatch: 'full' }, // default route
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: '**', component: NotFoundComponent } 
+];
